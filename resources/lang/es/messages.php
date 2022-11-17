409ea882b29ba0b5e87fd18fadcd4415138e53a4
jtirado1996 <jorge.tirado@conexionbox.com>
2022-11-17 16:27:52 -0600

    Spanish messages

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -7,6 +7,16 @@ return [
     'latest_groups' => 'Ultimos grupos',
     'categories' => 'Categorias',
     'copy_link' => 'Copiar link de grupo',
-    'links_error' => 'El vinculo tiene un fomato incorrecto'
-
+    'enter_group' => 'Unirte a grupo',
+    'form_create_group' => [
+        'title' => 'Crear titulo',
+        'description' => 'Crear descripcion',
+        'link' => 'Liga de grupo',
+        'image' => 'Imagen de grupo',
+        'category' => 'Categoria de grupo',
+        'messenger_brand' => 'Provedor de mensajería',
+    ],
+    'group_success' => 'Grupo creado exitosamente',
+    'links_error' => 'El vinculo tiene un fomato incorrecto',
+    'create_new_group' => 'Crear nuevo grupo',
 ];
